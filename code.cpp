f3bbb1c4f447f01b2a5aa504940f6cc5f7713466
Ulok <elfbrus@gmail.com>
2022-02-11 09:19:00 +0300

    ded

---
+++ code.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void func(float x) {
+  x = 
+  return x;
+}
+
+int main(int argc, char const *argv[]) {
+  setlocale(LC_ALL, "ru");
+  switch (/* expression */) {
+    case /* value */:
+  }
+  return 0;
+}
